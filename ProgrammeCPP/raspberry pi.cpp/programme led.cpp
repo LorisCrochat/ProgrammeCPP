c968503633a97878b7dcb259b8c9de778a84fa2a
Loris CROCHAT <crochat.loris@cdfnancy.fr>
2024-02-08 08:29:54 +0100

    led gpio

---
+++ ProgrammeCPP/raspberry pi.cpp/programme led.cpp
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <bcm2835.h>
+
+#define LED_PIN RPI_GPIO_P1_18 // GPIO pin 18
+
+int main() {
+    // Initialiser la bibliothèque BCM2835
+    if (!bcm2835_init()) {
+        return 1;
+    }
+
+    // Configurer la broche GPIO comme sortie
+    bcm2835_gpio_fsel(LED_PIN, BCM2835_GPIO_FSEL_OUTP);
+
+    // Allumer la LED
+    bcm2835_gpio_set(LED_PIN);
+
+    // Laisser la LED allumée pendant 5 secondes
+    sleep(5);
+
+    // Éteindre la LED
+    bcm2835_gpio_clr(LED_PIN);
+
+    // Terminer la bibliothèque BCM2835
+    bcm2835_close();
+
+    return 0;
+}
