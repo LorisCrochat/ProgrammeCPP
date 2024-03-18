28398fb5d08cedef24d6ba21ffeb843e638ccf7b
LorisCrochat <146432109+LorisCrochat@users.noreply.github.com>
2024-03-18 14:49:45 +0100

    exer1

---
+++ ProgrammeCPP/ProgrammeVR/tableau et chaine/exercice 1.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int nbValPaires(int *tab)
+{
+    int compteur = 0;
+    for (int i = 0; i < 10; i++)
+    {
+        if (tab[i] % 2 == 0)
+        {
+            compteur++;
+        }
+    }       
+    return compteur;
+      
+      cin.get();
+    cin.ignore();
+    return EXIT_SUCCESS;
+
+
+}
