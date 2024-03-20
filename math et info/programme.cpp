56ba667016b4aa74b0e63c20e70b5cca45c52f28
LorisCrochat <146432109+LorisCrochat@users.noreply.github.com>
2024-03-20 15:57:29 +0100

    maths

---
+++ math et info/programme.cpp
@@ -0,0 +1,17 @@
+#include <cmath>
+
+double f(double x) {
+    return 3 * log(1 + x * exp(-x));
+}
+
+double aire(int n) {
+    double A = 0;
+    double x = 0;
+
+    for (int i = 1; i <= n; i++) {
+        A += (1.0 / n) * (f(x) + f(x + 1.0 / n)) / 2;
+        x += 1.0 / n;
+    }
+
+    return A;
+}
---
+++ math et info/programme.py
@@ -0,0 +1,12 @@
+from math import
+
+def f(x):
+    return 3*log(1+x*exp(-x))
+
+def aire(n)
+     A=0
+     X=0
+for i in range(1,n+1):
+    A=A+(1/n)*(f(x)+f(x)+1/n)/2
+    x=x+1/n
+    return A 
